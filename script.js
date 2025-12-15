78df3e1f69f3e2d24235533012f09e24d6afee0b
Akannehinata-create <salmahjapose@gmail.com>
2025-12-15 21:41:48 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// LAYAR PEMUATAN
-window.onload = function() {
-document.getElementById("loading").style.display = "none";
-
-};
-
-// MODE GELAP
-document.getElementById("darkBtn").onclick = () => {
-document.body.cl
